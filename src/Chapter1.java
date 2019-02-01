bfce2533896cbca73031a9f3f8c467190fa0473c
Abshir <abshirm@live.com>
2019-02-01 11:08:20 -0800

    chapter 1 algos added

--- src/Chapter1.java
+++ src/Chapter1.java
@@ -1,4 +1,63 @@
-
+//This page contains most of the algorithms in Chapter 1 of the Coding Dojo Algorithms Book
 public class Chapter1 {
-
+	
+	public static void main(String[] args)
+	{
+		System.out.println(threesFives());
+	}
+	
+	public static int sigma(int num)
+	{
+		int sum = 0;
+		while(num > 0)
+			sum+=num--;
+		return sum;
+	}
+	
+	public static int factorial(int num)
+	{
+		int factor = 1;
+		
+		while(num > 0)
+			factor = num-- * factor++;
+		
+		return factor;
+	}
+	
+	public static int threesFives()
+	{
+		int sum = 0;
+		
+		for(int i = 100; i < 4000000 ; i++)
+			if (i % 5 == 0 || i % 3 == 0)
+				sum += i;
+				
+		
+		return sum;
+	}
+	
+	public static void generateCoinChange(int money)
+	{
+		int quarters = money / 25;
+		
+		money = money % 25;
+		
+		int dimes = money / 10;
+		
+		money = (money%10);
+		
+		int nickels = money / 5;
+		
+		money = money % 5;
+		
+		int pennies  = money;
+		
+		System.out.println("Quarters: " +);
+		
+		
+		
+		
+	}
+	
+	
 }
